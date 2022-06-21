d78fcc77fbdcf7d009770c526d633a168e32580f
mcsepro <aremsony2k4@yahoo.com>
2022-06-21 13:41:08 +0100

    task 0x06-python-classes created

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,80 +1,120 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
 
 class Square:
-    """
-    creates square
-    """
+    """Represents a square
 
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        """
-        initializes square
+        """initializes the square
 
         Args:
-            size: size of side of square
+            size (int): size of a side of the square
+
+        Returns:
+            None
         """
-        self.__size = size
+        self.size = size
+
+    def area(self):
+        """calculates the square's area
+
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
 
     @property
     def size(self):
-        """
-        finds size
+        """getter of __size
+
+        Returns:
+            The size of the square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        validates size is an integer that is greater than zero
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+        """setter of __size
 
-    def area(self):
-        """
-        finds area of square
+        Args:
+            value (int): the size of a size of the square
 
         Returns:
-            area of square
+            None
         """
-        return self.__size ** 2
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def __lt__(self, other):
+        """Compare if square is less than another by area
+
+        Args:
+            other (Square): square to compare against
+        Returns:
+            True or False
+        """
+        return self.size < other.size
+
+    def __le__(self, other):
+        """Compare if square is less than or equal to another by area
+
+        Args:
+            other (Square): square to compare against
+
+        Returns:
+            True or False
+        """
+        return self.size <= other.size
 
     def __eq__(self, other):
-        """
-        check if square is equal to other square
+        """Compare if square is equal to another by area
+
+        Args:
+            other (Square): square to compare against
+
+        Returns:
+            True or False
         """
         return self.size == other.size
 
     def __ne__(self, other):
-        """
-        check if square is not equal to other square
+        """Compare if square is not equal to another by area
+
+        Args:
+            other (Square): square to compare against
+
+        Returns:
+            True or False
         """
         return self.size != other.size
 
-    def __gt__(self, other):
-        """
-        check if square is greater than other square
-        """
-        return self.size > other.size
-
-    def __lt__(self, other):
-        """
-        check if square is less than other square
-        """
-        return self.size < other.size
-
     def __ge__(self, other):
-        """
-        check if square is greater than or equal to other square
+        """Compare if square is greater than or equal to another by area
+
+        Args:
+            other (Square): square to compare against
+
+        Returns:
+            True or False
         """
         return self.size >= other.size
 
-    def __le__(self, other):
+    def __gt__(self, other):
+        """Compare if square is greater than another by area
+
+        Args:
+            other (Square): square to compare against
+
+        Returns:
+            True or False
         """
-        check if square is less than or equal to other square
-        """
-        return self.size <= other.size
+        return self.size > other.size
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,44 +1,55 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
 
 class Square:
-    """
-    creates square
-    """
+    """Represents a square
 
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        """
-        initializes square
+        """initializes the square
 
         Args:
-            size: size of side of square
+            size (int): size of a side of the square
+
+        Returns:
+            None
         """
-        self.__size = size
+        self.size = size
+
+    def area(self):
+        """calculates the square's area
+
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
 
     @property
     def size(self):
-        """
-        finds size
+        """getter of __size
+
+        Returns:
+            The size of the square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        validates size is an integer that is greater than zero
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+        """setter of __size
 
-    def area(self):
-        """
-        finds area of square
+        Args:
+            value (int): the size of a size of the square
 
         Returns:
-            area of square
+            None
         """
-        return self.__size ** 2
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
