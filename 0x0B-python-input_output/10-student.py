94c6284186455ef4f5810681ccd467ab423892af
mcsepro <aremsony2k4@yahoo.com>
2022-07-05 13:57:53 +0100

    Task 0x0B created

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
+"""Student module.
+
+Contains a Student class and some methods.
 """
 
 
-class Student:
-    """Representation of a student"""
+class Student():
+    """Defines a Student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """Sets the necessary attributes for the Student object.
+
+        Args:
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
+"""Add item script."""
+import sys
 
-from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-
-filename = "add_item.json"
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    json_list = load_from_json_file(filename)
+    lst = load_from_json_file("add_item.json")
 except:
-    json_list = []
+    lst = []
 
-for arg in argv[1:]:
-    json_list.append(arg)
+argc = len(sys.argv)
 
-save_to_json_file(json_list, filename)
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
+
+save_to_json_file(lst, "add_item.json")
