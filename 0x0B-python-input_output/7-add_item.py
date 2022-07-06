7f3978918993a2871e7d1213f61c986a82512ce5
mcsepro <aremsony2k4@yahoo.com>
2022-07-06 09:45:01 +0100

    Task 0x0B created

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""add item"""
+""" contains the add_item function """
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-
-import json
-import sys
-import os.path
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-if os.path.isfile(filename):
-    obj = load_from_json_file(filename)
-else:
-    obj = []
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, filename)
+try:
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+for arg in argv[1:]:
+    my_list.append(arg)
+save_to_json_file(my_list, filename)
