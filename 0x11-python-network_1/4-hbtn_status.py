fd93c1b00d3bb55cdce1b23abd3920cf122e8e94
mcsepro <aremsony2k4@yahoo.com>
2022-09-23 15:00:40 +0100

    task 0x011 created

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+fetches https://intranet.hbtn.io/status
 """
-import requests
-
-
-if __name__ == "__main__":
-    res = requests.get('https://intranet.hbtn.io/status').text
+if __name__ == '__main__':
+    import requests
+    r = requests.get('https://intranet.hbtn.io/status')
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(res)))
-    print("\t- content: {}".format(res))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
