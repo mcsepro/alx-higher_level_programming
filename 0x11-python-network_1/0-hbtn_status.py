398ac69fa01fb8939a873c99d8156675aa49ebfb
mcsepro <aremsony2k4@yahoo.com>
2022-09-26 08:47:25 +0100

    task 0x11 updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ fetches https://intranet.hbtn.io/status
 """
 if __name__ == "__main__":
     import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
+    with request.urlopen('http://0.0.0.0:5050/status') as r:
         html = r.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "http://0.0.0.0:5050/status"
     r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
